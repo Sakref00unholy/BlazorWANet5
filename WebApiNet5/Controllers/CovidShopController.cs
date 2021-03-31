c949c8be401d4efaaea39f8b299ef4e5a2041e6a
Sakref00unholy <phillipmayland@gmail.com>
2021-03-31 16:42:03 +0200

    Initial commit

---
+++ WebApiNet5/Controllers/CovidShopController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CovidShopController : ControllerBase
+    {
+        private static List<CovidShop> shopList = new List<CovidShop>()
+        {
+            new CovidShop(1, 12, 45,"Pancakes"),
+            new CovidShop(2,2, 12,"Tea Shop")
+        };
+
+        // GET: api/shop
+        [HttpGet]
+        public List<CovidShop> Get()
+        {
+            return shopList;
+        }
+
+        // GET: api/shop/5
+        // [HttpGet("{id}", Name = "Get")]
+        [HttpGet]
+        [Route("{id}")]
+        public CovidShop GetshopWithId(int id /*int id*/)
+        {
+            return shopList.Find(i => i.Id == id);
+        }
+
+        // POST: api/shop
+        [HttpPost]
+        public CovidShop Post([FromBody] CovidShop shop)
+        {
+            shopList.Add(shop);
+            return shop;
+        }
+
+
+        // PUT: api/Covid/5
+        [Route("{id}")]
+        public bool Put(int id, [FromBody] CovidShop shop)
+        {
+            CovidShop selectedShop = GetshopWithId(id);
+            if (selectedShop != null)
+            {
+                selectedShop.Id = shop.Id;
+                selectedShop.Antal = shop.Antal;
+                selectedShop.AntalMax = shop.AntalMax;
+                selectedShop.ButiksNavn = shop.ButiksNavn;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ WebApiNet5/CovidShop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNet5
+{
+    public class CovidShop
+    {
+        public int Id { get; set; }
+        public int Antal { get; set; }
+        public int AntalMax { get; set; }
+        public string ButiksNavn { get; set; }
+
+        public CovidShop(int id, int antal, int antalMax, string butiksNavn)
+        {
+            Id = id;
+            Antal = antal;
+            AntalMax = antalMax;
+            ButiksNavn = butiksNavn;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Antal)}: {Antal}, {nameof(AntalMax)}: {AntalMax}, {nameof(ButiksNavn)}: {ButiksNavn}";
+        }
+    }
+}
